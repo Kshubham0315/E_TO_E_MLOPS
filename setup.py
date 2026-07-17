68531653e3b79f666e10c28c415d6b4565cd8f23
Shubham Kumar <kshubham0315@gmail.com>
2026-07-17 16:21:03 +0530

    structure of complete project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is the complete end to end mlops project for any project of aiml',
+    author='shubham kumar',
+    license='MIT',
+)
