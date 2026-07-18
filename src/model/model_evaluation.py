bb932b8cddf2ca1243c7049864ef89ccd0450f65
Shubham Kumar <kshubham0315@gmail.com>
2026-07-18 13:35:59 +0530

    Till here done

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -135,10 +135,10 @@ def main():
                     mlflow.log_param(param_name, param_value)
             
             # Log model to MLflow
-            mlflow.sklearn.log_model(clf, "model")
+            mlflow.sklearn.log_model(clf, "models")
             
             # Save model info
-            save_model_info(run.info.run_id, "model", 'reports/experiment_info.json')
+            save_model_info(run.info.run_id, "models", 'reports/experiment_info.json')
             
             # Log the metrics file to MLflow
             mlflow.log_artifact('reports/metrics.json')
