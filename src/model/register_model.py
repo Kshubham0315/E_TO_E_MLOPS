325181e017ba549cff2cb979f3af27e6264b2ec1
Shubham Kumar <kshubham0315@gmail.com>
2026-07-18 16:55:49 +0530

    Evething done

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -18,6 +18,8 @@ dagshub_token = os.getenv("CAPSTONE_TEST")
 if not dagshub_token:
     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
+
+
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
